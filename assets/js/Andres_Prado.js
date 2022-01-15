9b3e2c7e65ba37c3dedce5b28655ad6b8169b7db
AndresPradoVelarde <67911568+AndresPradoVelarde@users.noreply.github.com>
2022-01-15 03:51:04 -0500

    commit

--- assets/js/Andres_Prado.js
+++ assets/js/Andres_Prado.js
@@ -7,6 +7,8 @@ function validarDatos(event) {
     var txtNombre = document.getElementById("idNombre");
     var txtApellido = document.getElementById("idApellido");
     var txtemail = document.getElementById("idEmail");
+    //var txtNumero = document.getElementById("idCantidadPer");
+    var txtFecha = document.getElementById("idFecha");
     var letra = /^[a-z ,.'-]+$/i;// letrasyespacio   ///^[A-Z]+$/i;// solo letras
     var correo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
@@ -15,8 +17,10 @@ function validarDatos(event) {
     validarNombre(txtNombre, resultado, letra, mensaje);
     validarApellido(txtApellido, resultado, letra, mensaje);
     validarCorreoElectronico(txtemail, resultado, correo, mensaje);
+    //validarNumero();
+    validarFecha(txtFecha, resultado, mensaje);
 
-    if (!validarNombre.resultado || !validarApellido.resultado || !validarCorreoElectronico.resultado) {
+    if (!validarNombre.resultado || !validarApellido.resultado || !validarCorreoElectronico.resultado || !validarFecha.resultado) {
         event.preventDefault();  // detener el evento  //stop form from submitting
     }
 }
@@ -57,6 +61,28 @@ function validarCorreoElectronico(txtemail, resultado, correo) {
         mensaje("Email no es correcto", txtemail);
     }
 }
+
+function validarFecha(txtFecha, resultado) {
+    // validacion de fecha
+    var dato = txtFecha.value;
+    var fechaN = new Date(dato);
+    var anioN = fechaN.getFullYear();
+
+    var fechaActual = new Date();// fecha actual
+    var anioA = fechaActual.getFullYear();
+    if (fechaN > fechaActual) {
+        resultado = false;
+        mensaje("Fecha no puede ser superior a la actual", txtFecha);
+    } else if (anioN < 1930) {
+        resultado = false;
+        mensaje("Anio de nacimiento no puede ser menor a 1930", txtFecha);
+    } else if ((anioA - anioN) < 18) {
+        resultado = false;
+        mensaje("debe ser mayor de 18 años", txtFecha);
+    }
+
+}
+
 
 function mensaje(cadenaMensaje, elemento) {
     elemento.focus();
